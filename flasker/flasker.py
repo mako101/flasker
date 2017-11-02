7548fa0117389e57f31d0b0af7422f27a5d39f01
Your Name <you@example.com>
2017-11-02 08:23:39 +0000

    wip

--- flasker/flasker.py
+++ flasker/flasker.py
@@ -28,3 +28,45 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        if error:
+            print('Got error on teardown: {}'.format(error))
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute(
+        'select title, text from entries order by id desc'
+    )
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
